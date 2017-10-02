04cdca02f2c479ff4f47d2d878c71e57baf9b103
YHJ <yhj2263>
2017-10-02 01:04:17 -0700

    commented out bug codes

--- src/classifier.py
+++ src/classifier.py
@@ -113,8 +113,8 @@ def main(args):
                     pickle.dump((model, class_names), outfile)
 
                 # Create a json object with classifier name and time created
-                # classifier_json = {'name': classifier_name, 'date': str(time.ctime())}
-                # append_to_json(classifier_json, classifier_index_exp)
+                classifier_json = {'name': classifier_name, 'date': str(time.ctime())}
+                append_to_json(classifier_json, classifier_index_exp)
 
                 print('Saved classifier model to file "%s"' % classifier_filename_exp)
                 print('Wrote classifier information to file "%s"' % classifier_index_exp)
@@ -185,20 +185,20 @@ def parse_arguments(argv):
 
     return parser.parse_args(argv)
 
-# def append_to_json(_dict,path):
-#     with open(path, 'a+') as f:
-#         # Go to the end of file
-#         f.seek(0,2)
-#         # Check if file is empty
-#         if f.tell() == 0 :
-#             # If empty, write an array
-#             json.dump([_dict], f, indent=4, separators=(',', ': '))
-#         else :
-#             f.seek(-1,2)
-#             f.truncate()           # Remove the last character, open the array
-#             f.write(',')         # Write the separator
-#             json.dump(_dict, f, indent=4, separators=(',', ': '))     # Dump the dictionary
-#             f.write(']')           # Close the array
+def append_to_json(_dict,path):
+    with open(path, 'a+') as f:
+        # Go to the end of file
+        f.seek(0,2)
+        # Check if file is empty
+        if f.tell() == 0 :
+            # If empty, write an array
+            json.dump([_dict], f, indent=4, separators=(',', ': '))
+        else :
+            f.seek(-1,2)
+            f.truncate()           # Remove the last character, open the array
+            f.write(',')         # Write the separator
+            json.dump(_dict, f, indent=4, separators=(',', ': '))     # Dump the dictionary
+            f.write(']')           # Close the array
 
 if __name__ == '__main__':
     main(parse_arguments(sys.argv[1:]))
